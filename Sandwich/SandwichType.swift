4e5ec29f53ecf9a084995ebef1b53482d16037d9
Jason Welch <jwelch@mindgrub.com>
2017-07-16 13:41:38 -0400

    Moved sandwich validation to SandwichType

--- Sandwich/SandwichType.swift
+++ Sandwich/SandwichType.swift
@@ -8,6 +8,41 @@
 
 import UIKit
 
+enum CraftingError: Error {
+    case invalidIngredients
+}
+
 enum SandwichType {
     case ham, veggie, breakfast, pbj, cheese
+
+    static func craft(from ingredients: [String]) throws -> SandwichType {
+
+        if ingredients.contains("cheese") {
+            return craftCheeseSandwich(from: ingredients)
+        } else if ingredients.contains("lettuce"),
+            ingredients.contains("tomato"),
+            ingredients.contains("egg") {
+
+            return .breakfast
+        } else if ingredients.contains("peanut_butter"),
+            ingredients.contains("jelly") {
+
+            return .pbj
+        }
+        throw CraftingError.invalidIngredients
+    }
+
+    private static func craftCheeseSandwich(from ingredients: [String]) -> SandwichType {
+        if ingredients.contains("lettuce"),
+            ingredients.contains("tomato") {
+
+            if ingredients.contains("ham") {
+                return .ham
+            } else if ingredients.contains("cucumber") {
+                return .veggie
+            }
+        }
+        
+        return .cheese
+    }
 }
